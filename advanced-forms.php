f1203aa96e0157b9bd46a900b35eb788d2f5903e
Fabian Lindfors <fabian@flapplabs.se>
2016-11-20 15:03:15 +0100

    Updated version and changelog

--- advanced-forms.php
+++ advanced-forms.php
@@ -2,7 +2,7 @@
 /*
 	Plugin Name: Advanced Forms
 	Description: Flexible and developer-friendly forms with the power of Advanced Custom Fields.
-	Version: 1.0.0
+	Version: 1.0.1
 	Author: Fabian Lindfors
 	Copyright: Fabian Lindfors
 	Text Domain: advanced-forms
