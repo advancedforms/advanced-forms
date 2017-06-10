45fef0fd974fde474ffe031951f3819819a48bc2
Fabian Lindfors <fabian@flapplabs.se>
2017-06-10 20:01:08 +0200

    Added global field setting for hiding fields from admin

--- acf/acf-additions.php
+++ acf/acf-additions.php
@@ -11,9 +11,54 @@ class AF_ACF_Additions {
 	
 	function __construct() {
 		
+		add_action('acf/render_field_settings', array( $this, 'field_settings'), 100 );
+		add_action('acf/prepare_field', array( $this, 'hide_field_from_admin'), 100 );
+		
 		add_filter( 'acf/location/rule_types', array( $this, 'add_form_location_type' ), 10, 1 );
 		add_filter( 'acf/location/rule_values/af_form', array( $this, 'form_location_rule_values' ), 10, 1 );
 		add_filter( 'acf/location/rule_match/af_form', array( $this, 'form_location_rule_match' ), 10, 3 );
+		
+	}
+	
+	
+	/**
+	 * Add extra global field settings
+	 *
+	 * @since 1.3.1
+	 *
+	 */
+	function field_settings( $field ) {
+		
+		acf_render_field_setting( $field, array(
+			'label'			=> __('Hide from admin?'),
+			'instructions'	=> 'Only show this field in forms',
+			'name'			=> 'hide_admin',
+			'type'			=> 'true_false',
+			'ui'			=> 1,
+		), true);
+		
+	}
+	
+	
+	/**
+	 * Hide fields with the "Hide from admin?" setting
+	 *
+	 * @since 1.3.1
+	 *
+	 */ 
+	function hide_field_from_admin( $field ) {
+		
+		if ( empty( $field['hide_admin'] ) ) {
+			return $field;
+		}
+		
+		
+		if ( $field['hide_admin'] && is_admin() ) {
+			return false;
+		}
+		
+		
+		return $field;
 		
 	}
 	
