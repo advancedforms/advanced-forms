6b14a06ca558fda0e441e54e9500459f0325e5f7
Fabian Lindfors <fabian@flapplabs.se>
2018-11-28 16:56:19 +0100

    Added gulp watch task and renamed build task

--- gulpfile.js
+++ gulpfile.js
@@ -3,32 +3,43 @@ var minifyCSS = require('gulp-csso');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 
+var formCSS = ['assets/css/form.css', 'pro/assets/css/form.css'];
+var adminCSS = ['assets/css/admin.css', 'pro/assets/css/admin.css'];
+var formJS = ['assets/js/forms.js', 'pro/assets/js/forms.js'];
+var adminJS = ['assets/js/admin.js', 'pro/assets/js/admin.js'];
+var all = [].concat(formCSS, adminCSS, formJS, adminJS);
+
 gulp.task('form-css', function(){
-  return gulp.src(['assets/css/form.css', 'pro/assets/css/form.css'])
+  return gulp.src(formCSS)
     .pipe(concat('form.css'))
     .pipe(minifyCSS())
     .pipe(gulp.dest('assets/dist/css'))
 });
 
 gulp.task('admin-css', function(){
-  return gulp.src(['assets/css/admin.css', 'pro/assets/css/admin.css'])
+  return gulp.src(adminCSS)
     .pipe(concat('admin.css'))
     .pipe(minifyCSS())
     .pipe(gulp.dest('assets/dist/css'))
 });
 
 gulp.task('forms-js', function(){
-  return gulp.src(['assets/js/forms.js', 'pro/assets/js/forms.js'])
+  return gulp.src(formJS)
     .pipe(concat('forms.js'))
     .pipe(uglify())
     .pipe(gulp.dest('assets/dist/js'))
 });
 
 gulp.task('admin-js', function(){
-  return gulp.src(['assets/js/admin.js', 'pro/assets/js/admin.js'])
+  return gulp.src(adminJS)
     .pipe(concat('admin.js'))
     .pipe(uglify())
     .pipe(gulp.dest('assets/dist/js'))
 });
 
-gulp.task('default', [ 'form-css', 'admin-css', 'forms-js', 'admin-js' ]);
+gulp.task('watch', function () {
+    // Endless stream mode
+    return gulp.watch(all, ['build']);
+});
+
+gulp.task('build', [ 'form-css', 'admin-css', 'forms-js', 'admin-js' ]);
