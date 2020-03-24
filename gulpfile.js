f13b21bf23b116aeca40328b8951009f21836f88
fabianlindfors <fabian@flapplabs.se>
2020-03-24 22:57:52 +0100

    Upgraded Gulp to v4

--- gulpfile.js
+++ gulpfile.js
@@ -10,28 +10,28 @@ var adminJS = ['assets/js/admin.js', 'pro/assets/js/admin.js'];
 var all = [].concat(formCSS, adminCSS, formJS, adminJS);
 
 gulp.task('form-css', function(){
-  return gulp.src(formCSS)
+  return gulp.src(formCSS, { allowEmpty: true })
     .pipe(concat('form.css'))
     .pipe(minifyCSS())
     .pipe(gulp.dest('assets/dist/css'))
 });
 
 gulp.task('admin-css', function(){
-  return gulp.src(adminCSS)
+  return gulp.src(adminCSS, { allowEmpty: true })
     .pipe(concat('admin.css'))
     .pipe(minifyCSS())
     .pipe(gulp.dest('assets/dist/css'))
 });
 
 gulp.task('forms-js', function(){
-  return gulp.src(formJS)
+  return gulp.src(formJS, { allowEmpty: true })
     .pipe(concat('forms.js'))
     .pipe(uglify())
     .pipe(gulp.dest('assets/dist/js'))
 });
 
 gulp.task('admin-js', function(){
-  return gulp.src(adminJS)
+  return gulp.src(adminJS, { allowEmpty: true })
     .pipe(concat('admin.js'))
     .pipe(uglify())
     .pipe(gulp.dest('assets/dist/js'))
@@ -39,7 +39,7 @@ gulp.task('admin-js', function(){
 
 gulp.task('watch', function () {
     // Endless stream mode
-    return gulp.watch(all, ['build']);
+    return gulp.watch(all, gulp.series('build'));
 });
 
-gulp.task('build', [ 'form-css', 'admin-css', 'forms-js', 'admin-js' ]);
+gulp.task('build', gulp.series('form-css', 'admin-css', 'forms-js', 'admin-js'));
