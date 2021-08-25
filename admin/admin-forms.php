2327eea4951385c9396644fc83a1510da2d91688
fabianlindfors <fabian@flapplabs.se>
2021-08-25 13:48:23 +0200

    Use filter to avoid duplicate form key meta

--- admin/admin-forms.php
+++ admin/admin-forms.php
@@ -7,14 +7,13 @@
  *
  */
 class AF_Admin_Forms {
-	
 	function __construct() {
-		
 		// Actions
 		add_action( 'admin_init', array( $this, 'add_fields_meta_box' ), 10, 0 );
 		add_action( 'edit_form_after_title', array( $this, 'display_form_key' ), 10, 0 );
 		add_filter( 'acf/prepare_field/name=form_shortcode_message', array( $this, 'display_form_shortcode' ), 10, 1 );
 		add_action( 'save_post', array( $this, 'add_form_key' ), 10, 3 );
+		add_filter( 'add_post_metadata', array( $this, 'should_add_form_key_meta' ), 10, 3 );
 		add_action( 'acf/init', array( $this, 'register_fields' ), 10, 0 );
 		add_action( 'media_buttons', array( $this, 'add_wysiwyg_content_field_inserter' ), 10, 1 );
 		add_action( 'admin_footer', array( $this, 'add_forms_sidebar' ), 10, 0 );
@@ -24,10 +23,7 @@ class AF_Admin_Forms {
 		add_filter( 'manage_af_form_posts_columns', array( $this, 'manage_columns' ), 10, 1 );
 		add_action( 'manage_af_form_posts_custom_column', array( $this, 'custom_columns_content' ), 10, 2 );
 		add_filter( 'disable_months_dropdown', array( $this, 'disable_months_filter' ), 10, 2 );
-		
 	}
-	
-	
 	
 	/**
 	 * Adds a form key to a form if one doesn't exist
@@ -36,17 +32,31 @@ class AF_Admin_Forms {
 	 *
 	 */
 	function add_form_key( $post_id, $post, $update ) {
-		
 		if ( 'af_form' == $post->post_type && ! get_post_meta( $post->ID, 'form_key', true ) ) {
-			
 			$form_key = 'form_' . uniqid();
-			
 			update_post_meta( $post->ID, 'form_key', $form_key );
-			
 		}
-		
 	}
-	
+
+	/**
+	 * Stops new form keys from being saved to a form post if a key already exists.
+	 * Some plugins that duplicate posts will cause trouble as forms will end up with multiple form keys.
+	 * 
+	 * @since 1.9.0
+	 * 
+	 */
+	function should_add_form_key_meta( $check, $object_id, $meta_key ) {
+		if ( 'form_key' !== $meta_key ) {
+			return $check;
+		}
+
+		// If a form key already exists, we don't want to save another one
+		if ( metadata_exists( 'post', $object_id, $meta_key ) ) {
+			return false;
+		}
+
+		return $check;
+	}
 	
 	/**
 	 * Displays the form key after the title
@@ -55,21 +65,14 @@ class AF_Admin_Forms {
 	 *
 	 */
 	function display_form_key() {
-		
 		global $post;
 		
 		if ( 'af_form' == $post->post_type && $form_key = get_post_meta( $post->ID, 'form_key', true ) ) {
-			
 			echo '<div id="edit-slug-box">';
-			
 			echo sprintf( '<strong>%s </strong>%s', __( 'Form key:', 'advanced-forms' ), $form_key );
-			
 			echo '</div>';
-			
 		}
-		
 	}
-
 
 	/**
 	 * Display the form shortcode in the form settings.
@@ -88,7 +91,6 @@ class AF_Admin_Forms {
 		return $field;
 	}
 	
-	
 	/**
 	 * Adds a meta box to the form edit page used to display the form fields
 	 * 
@@ -96,11 +98,8 @@ class AF_Admin_Forms {
 	 *
 	 */
 	function add_fields_meta_box() {
-		
 		add_meta_box( 'af_form_fields', __( 'Fields', 'advanced-forms' ), array( $this, 'fields_meta_box_callback' ), 'af_form', 'normal', 'default', null );
-		
 	}
-	
 	
 	/**
 	 * Callback for the fields meta box
@@ -110,14 +109,12 @@ class AF_Admin_Forms {
 	 *
 	 */
 	function fields_meta_box_callback() {
-		
 		global $post;
 		
 		$form = af_get_form( $post->ID );
 		
 		// Get field groups for the current form
 		$field_groups = af_get_form_field_groups( $form['key'] );
-		
 		?>
 		
 		<p><?php _e( 'Add fields by setting the location of your fields group to this form.', 'advanced-forms' ); ?></p>
@@ -130,36 +127,26 @@ class AF_Admin_Forms {
 					<th scope="col"><?php _e( 'Type', 'advanced-forms' ) ?></th>
 				</tr>
 			</thead>
-			
 			<tbody>
-				
 				<?php if ( ! empty( $field_groups ) ) : ?>
-				
 					<?php foreach ( $field_groups as $field_group ) : ?>
-					
 						<?php
 							// Get all fields for this field group
 							$fields = acf_get_fields( $field_group );	
 						?>
-						
 						<tr class="field-group-heading">
 							<td colspan="3">
 								<a href="<?php echo get_edit_post_link( $field_group['ID'] ); ?>"><?php echo $field_group['title']; ?></a>
 							</td>
 						</tr>
-											
 						<?php foreach ( $fields as $field ) : ?>
-							
 							<tr>
 								<td><?php echo $field['label']; ?></td>
 								<td><?php echo $field['name']; ?></td>
 								<td><?php echo acf_get_field_type_label( $field['type'] ); ?></td>
 							</tr>
-						
 						<?php endforeach; ?>
-						
 					<?php endforeach; ?>
-					
 				<?php else: ?>
 					<tr>
 						<td colspan="3">
@@ -167,16 +154,13 @@ class AF_Admin_Forms {
 						</td>
 					</tr>
 				<?php endif; ?>
-				
 			</tbody>
 		</table>
-
 		<a href="<?php echo admin_url( 'post-new.php?post_type=acf-field-group' ); ?>" class="button">
 			<?php _e( 'Create field group', 'advanced-forms' ); ?>
 		</a>
 		<?php
 	}
-	
 	
 	/**
 	 * Adds custom columns to the listings page
@@ -185,7 +169,6 @@ class AF_Admin_Forms {
 	 *
 	 */
 	function manage_columns( $columns ) {
-		
 		$new_columns = array(
 			'key'		=> __( 'Key', 'advanced-forms' ),
 			'fields' 	=> __( 'Fields', 'advanced-forms' ),
@@ -196,9 +179,7 @@ class AF_Admin_Forms {
 		unset( $columns['date'] );
 		
 		return array_merge( array_splice( $columns, 0, 2 ), $new_columns, $columns );
-		
 	}
-	
 	
 	/**
 	 * Outputs the content for the custom columns
@@ -207,38 +188,25 @@ class AF_Admin_Forms {
 	 *
 	 */
 	function custom_columns_content( $column, $post_id ) {
-		
 		$form = af_get_form( $post_id );
 		
 		if ( 'key' == $column ) {
-			
 			echo get_post_meta( $post_id, 'form_key', true );
-			
-		} if ( 'fields' == $column ) {
-			
+		} elseif ( 'fields' == $column ) {
 			$count = 0;
 			$field_groups = af_get_form_field_groups( $form['key'] );
 			
 			// Count the number of fields in all field groups
 			foreach ( $field_groups as $field_group ) {
-				
 				$fields = acf_get_fields( $field_group );
 				$count += count( $fields );
-				
 			}
-			
 			echo $count;
-			
 		} elseif ( 'entries' == $column ) {
-			
 			$entries = af_get_entries( $form['key'] );
-			
 			echo sprintf( '<a href="%s">%s</a>', admin_url() . '/edit.php?post_type=af_entry&entry_form=' . $form['key'], count( $entries ) );
-			
 		}
-		
 	}
-	
 	
 	/**
 	 * Add an "Insert field" button to WYSIWYG fields
@@ -247,30 +215,22 @@ class AF_Admin_Forms {
 	 *
 	 */
 	function add_wysiwyg_content_field_inserter( $id ) {
-		
 		global $post;
 		
 		if ( ! $post ) {
 			return;
 		}
 		
-		
 		$form = af_form_from_post( $post );
-		
 		if ( ! $form ) {
 			return;
 		}
 		
-		
 		if ( 'acf-editor' == substr($id, 0, 10) ) {
-			
 			_af_field_inserter_button( $form, 'all', false );
-			
 		}
 		
-		
 	}
-
 
 	/**
 	 * Outputs the sidebar on the forms list page
@@ -326,7 +286,6 @@ class AF_Admin_Forms {
 		<?php
 	}
 
-
 	/**
 	 * Adds an wrapper for action buttons to the publish box.
 	 * Refactored out of admin-preview.php.
@@ -335,19 +294,14 @@ class AF_Admin_Forms {
 	 *
 	 */
 	function add_actions( $post ) {
-
 		if ( empty( $post ) || 'af_form' != $post->post_type ) {
-      return;
-    }
+    		return;
+    	}
 
-    echo '<div class="af-form-actions-wrapper">';
-
-    do_action( 'af/admin/form/actions', $post->ID );
-
-    echo '</div>';
-
+    	echo '<div class="af-form-actions-wrapper">';
+    	do_action( 'af/admin/form/actions', $post->ID );
+    	echo '</div>';
 	}
-
 
 	/**
 	 * Hides the months filter on the forms listing page.
@@ -356,13 +310,12 @@ class AF_Admin_Forms {
 	 *
 	 */
 	function disable_months_filter( $disabled, $post_type ) {
-    if ( 'af_form' != $post_type ) {
-      return $disabled;
-    }
+    	if ( 'af_form' != $post_type ) {
+    	  return $disabled;
+    	}
 
-    return true;
-  }
-
+    	return true;
+  	}
 
 	/**
 	 * Registers the form settings fields
@@ -371,7 +324,6 @@ class AF_Admin_Forms {
 	 *
 	 */
 	function register_fields() {
-		
 		$settings_field_group = array (
 			'key' => 'group_form_settings',
 			'title' => __( 'Form settings', 'advanced-forms' ),
@@ -514,13 +466,9 @@ class AF_Admin_Forms {
 			'description' => '',
 		);
 		
-		
 		$settings_field_group = apply_filters( 'af/form/settings_fields', $settings_field_group );
-		
 		acf_add_local_field_group( $settings_field_group );
-		
 	}
-	
 }
 
 return new AF_Admin_Forms();
