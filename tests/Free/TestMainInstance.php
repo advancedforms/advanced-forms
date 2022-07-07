e698779e6a9667df230e82228d889d93318cbeb9
Phil Kurth <phil@philkurth.com.au>
2022-07-07 10:27:10 +1000

    rename test method

--- tests/Free/TestMainInstance.php
+++ tests/Free/TestMainInstance.php
@@ -6,7 +6,7 @@ use WP_UnitTestCase;
 
 class TestMainInstance extends WP_UnitTestCase {
 
-	public function test_main_instance_pro_flag_returns_false() {
+	public function test_pro_flag_returns_false() {
 		$this->assertFalse( AF()->pro );
 	}
 
